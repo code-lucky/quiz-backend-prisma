6d0c0c5267b3e36a4705dcdf21c0cb80ae507efc
code-lucky <2924249993@qq.com>
2024-10-30 08:27:11 +0800

    update

--- apps/frontend/src/modules/file/file.service.ts
+++ apps/frontend/src/modules/file/file.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFileDto } from './dto/create-file.dto';
-import * as puppeteer from 'puppeteer';
 
 @Injectable()
 export class FileService {
@@ -10,23 +9,5 @@ export class FileService {
 
   async getFile(filename: string) {
     //
-  }
-
-  async generateImage(htmlContent: string): Promise<Buffer> {
-    try{
-      const browser = await puppeteer.launch({
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-      });
-      const page = await browser.newPage();
-  
-      await page.setContent(htmlContent);
-      const buffer = (await page.screenshot({ type: 'png' })) as Buffer;
-  
-      await browser.close();
-      return buffer;
-    } catch (error) {
-      console.error('Error generating image:', error);
-      throw new Error('Failed to generate image');
-    }
   }
 }
